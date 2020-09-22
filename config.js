02ed2d3604fdcfb5be46197d0734f771baa06ab3
hanuman <khanuman.109@gmail.com>
2020-09-22 21:52:01 +0300

    sec com

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: process.env.PORT || 5000,
+    PORT: process.env.PORT || 80,
     MONGO_URL: 'mongodb+srv://khanuman:4815162342omOM@incogram.s8ctm.mongodb.net/incogram?retryWrites=true&w=majority',
 }
